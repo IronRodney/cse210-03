cf645e66108916dd3f0a4293a138012e71abc73e
ogbaududavid <ogbaudu.dc@gmail.com>
2022-10-24 11:29:58 +0100

    final code

--- jumper/game/winner.py
+++
@@ -1,7 +0,0 @@
-"""In construction: we need a class that will be able to terminate the game 
-once the user has been able to guess the puzzle word correctly"""
-class Winner:
-    def __init__(self):
-        for sting in guesses:
-            if string in word:
-
